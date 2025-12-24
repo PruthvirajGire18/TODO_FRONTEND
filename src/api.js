c1e7aedd1accffbad42ad33a40eb0da4d0e26498
PruthvirajGire18 <girepruthviraj@gmail.com>
2025-12-24 15:11:10 +0530

    url changed

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://todo-backend-ivory-five.vercel.app", // ✅ your backend base URL
+  baseURL: "https://todo-backend-ivory-five.vercel.app/api", // ✅ your backend base URL
   withCredentials: true, // ✅ allow cookies/sessions if needed
 });
 
